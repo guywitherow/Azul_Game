b22f58cf72c205960e42658849c573f0ea4c6ad8
Guy Witherow s3783428 <s3783428@student.rmit.edu.au>
2020-05-01 14:09:17 +1000

    added makefile and menu message

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,22 @@
+#include <iostream>
+#include <string>
 
-
+void printMenu();
 
 int main(int argc, char const *argv[])
 {
-   /* code */
+   printMenu();
+   
    return 0;
+}
+
+void printMenu() {
+
+   std::string menu[] = {"Welcome to Azul!","-------------------","Menu","----","1. New Game",
+                     "2. Load Game","3. Credits (Show student information)","4. Quit"};
+   int menuSize = 8;
+
+   for (int i = 0; i < menuSize; i++) {
+      std::cout << menu[i] << std::endl;
+   }
 }
