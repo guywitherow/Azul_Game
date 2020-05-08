0aad36ba4a3fb2c3cb93e7069df9f3739cdd1504
s3668396 <53162933+s3668396@users.noreply.github.com>
2020-05-09 03:02:06 +1000

    Added LinkedList header class
    Finshed LinkedList header class with corresponding classes that were made for it. Will work on it and finish it by tomorrow midday hopefully

---
+++ LinkedList.h
@@ -0,0 +1,48 @@
+#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include "TileNode.h"
+#include "Tile.h"
+
+class LinkedList 
+{
+//Consructor
+LinkedList();
+
+//Deconstructor
+~LinkedList();
+
+//will be used in Deconstructor
+void clear();
+
+//Returns the size of the linked list 
+int getSize();
+
+//This returns the tile in the linked list based on the index 
+Tile* getTile(int index);
+
+//Based on the index a Tile in the linked list will return this toString
+std::string getTileToString(int index);
+
+//Adding a tile on either front or back of linked list
+void addFront(Tile* tileInfo);
+void addBack(Tile* tileinfo);
+
+//Deleting/Removing a tile from a linked list
+void deleteAt(int index);
+void deleteFront();
+void deleteBack();
+
+//Transferring a tile from one linked list to another
+void transferTo(int index, LinkedList* list);
+Tile* transferFront();
+Tile* transferBack();
+
+private:
+TileNode* head;
+TileNode* Tail;
+
+//The Temp transfer for when a tile that's used to define the tile to be transferred from one linked list to another
+Tile* transferTemp;
+};
+#endif //LINKEDLIST_H//
---
+++ TileNode.h
@@ -0,0 +1,11 @@
+#ifndef TILENODE_H
+#define TILENODE_H
+
+#include "Tile.h"
+
+class TileNode
+{
+    
+}
+
+#endif // TILENODE_H
